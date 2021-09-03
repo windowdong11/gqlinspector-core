f709d7c3e6d834190b06627c494bf54352a5ef1b
windowdong11 <windowdong11@naver.com>
2021-09-03 20:37:38 +0900

    Remove unused dependency yargs

--- index.ts
+++ index.ts
@@ -2,8 +2,6 @@
 import { IntrospectionSchema } from "graphql";
 import { getSchemaFromEndpoint } from "./getSchema";
 import { analyzeSchemaByType } from "./inspector";
-import yargs from 'yargs'
-import { hideBin } from 'yargs/helpers'
 
 export default function inspectSchemaFromEndpoint(endpoint: string) {
     return getSchemaFromEndpoint(endpoint)
